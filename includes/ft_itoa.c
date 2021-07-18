7f85562b6adb9471a5ea16c42572e02056edf963
luis <lp_santos29@outlook.com>
2021-07-18 02:30:47 -0300

    Update

---
+++ includes/ft_itoa.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaulo-d <lpaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/28 19:33:19 by lpaulo-d          #+#    #+#             */
+/*   Updated: 2021/07/18 02:28:05 by lpaulo-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	int			sign;
+	int			size;
+	char		*conv;
+	long int	nn;
+
+	sign = n < 0;
+	nn = n;
+	if (sign > 0)
+		nn = -nn;
+	size = 1;
+	while (n / 10 != 0 && size++)
+		n /= 10;
+	conv = malloc(sizeof(char) * (size + sign + 1));
+	if (!conv)
+		return (NULL);
+	conv[size + sign] = '\0';
+	while (size-- != 0)
+	{
+		conv[size + sign] = nn % 10 + '0';
+		nn /= 10;
+	}
+	if (sign > 0)
+		conv[0] = '-';
+	return (conv);
+}
