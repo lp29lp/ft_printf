c37748ac37c82e0019d44183d3b6938b39eab436
luis <lp_santos29@outlook.com>
2021-07-20 11:17:09 -0300

    Update

--- src/print_diu.c
+++ src/print_diu.c
@@ -18,7 +18,7 @@ static int check_sign(int num, t_guide *guide, const char *text);
 void	print_diu(const char *text, va_list args, t_guide *guide)
 {
 	int num;
-	unsigned int unum;
+	unsigned long int unum;
 	int size;
 	char *src;
 	int count = 0;
@@ -27,12 +27,12 @@ void	print_diu(const char *text, va_list args, t_guide *guide)
 	if (text[guide->i] == 'd' || text[guide->i] == 'i')
 		num = va_arg(args, int);
 	else
-		unum = va_arg(args, unsigned int);
-	if (num < 0 && text[guide->i] == 'd' || text[guide->i] == 'i')
+		unum = va_arg(args, unsigned long int);
+	if ((num < 0 && text[guide->i] == 'd') || (num < 0 && text[guide->i] == 'i'))
 		num = check_sign(num, guide, text);
 	if (text[guide->i] == 'd' || text[guide->i] == 'i')
 		src = ft_itoa(num);
-	if (text[guide->i] == 'u' && num != 0)
+	if (text[guide->i] == 'u' && unum != 0)
 		src = ft_utoa(unum);
 	size = ft_strlen(src);
 	if (size == 0)
